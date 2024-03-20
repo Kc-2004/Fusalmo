9359c3dce1636c702440a66dc3d6b61d28ac5e1a
Kc-2004 <91918257+Kc-2004@users.noreply.github.com>
2024-03-19 20:41:47 -0600

    Add files via upload

---
+++ TiendaRoutes/src/data/dataCel.jsx
@@ -0,0 +1,23 @@
+const dataCel={
+    Samsung:{
+      modelo: "A54",
+      precio: "$400.00",
+      desc: "Resistente al agua "
+    } ,
+    LG:{
+      modelo: "ThinQ v60",
+      precio: "$450.00",
+      desc: "Antivirus "
+    }, 
+    Xiaomi:{
+      modelo: "Redmi note 9t",
+      precio: "$500.00",
+      desc: "Bateria duradera "
+    },
+    iPhone:{
+      modelo: "XR",
+      precio: "$999.00",
+      desc: "Camara de alta gama"
+    }  
+  };
+export default dataCel;
